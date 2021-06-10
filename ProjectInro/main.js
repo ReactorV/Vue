a15360bdd8683e46d3f82ae0524b9e77639bf9d8
YRAL375 <vvoitkus@mail.ru>
2021-06-10 23:41:36 +0300

    V-bind and if else directives

---
+++ ProjectInro/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A good socks for developers',
+        image: './green-socks.png',
+        altText: 'A pair of socks',
+        onSale: true,
+        inventory: 2
+    }
+})
