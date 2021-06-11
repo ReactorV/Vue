a463aa1bd866e8acb631f9d3d590162e44faaa61
Vadim Voytkus <vadim.voytkus@amtote.com>
2021-06-11 23:13:48 +0300

    Event handling

--- ProjectInro/main.js
+++ ProjectInro/main.js
@@ -6,6 +6,34 @@ const app = new Vue({
         image: './green-socks.png',
         altText: 'A pair of socks',
         onSale: true,
-        inventory: 2
+        inventory: 2,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './green-socks.png'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: './blue-socks.png'
+            }
+        ],
+        sizes: ["XS", "S", "M", "L", "XL"],
+        cart: 0
+    },
+    methods: {
+        addToCart () {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            if(this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+        updateProduct(product) {
+            this.image = product
+        }
     }
 })
